49d1c6207795b64e79a78f10efb12aa229fa41d6
badajkawa <45787924+badajkawa@users.noreply.github.com>
2018-12-13 08:57:09 +0700

    Init layer (#6)
    * add application directory
    
    * move to application
    
    * fix mised requeire routes

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./application/routes/index');
+var usersRouter = require('./application/routes/users');
 
 var app = express();
 
