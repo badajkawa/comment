eab5ae4a5576f29508dd50a6f123c2b7af1feaf7
badajkawa <badajkawa@gmai.com>
2018-12-13 16:27:05 +0700

    index nsq

---
+++ mq/index.js
@@ -0,0 +1,23 @@
+
+
+const nsq = require('nsqjs')
+
+const reader = new nsq.Reader('instantloan__apply_loan', 'instantloan__apply', {
+    nsqdTCPAddresses: '127.0.0.1:4150'
+})
+
+
+reader.connect()
+
+
+
+reader.on('message', msg => {
+    //console.log('Received message [%s]: %s', msg.id, msg.body.toString())
+    console.log('Received message')
+    msg.finish()
+})
+
+
+
+
+
