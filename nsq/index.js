63bbd8ca362bd5cad49fbb5ed281bb55b9fc023f
badajkawa <badajkawa@gmail.com>
2018-12-14 14:21:56 +0700

    tu

--- nsq/index.js
+++ nsq/index.js
@@ -3,10 +3,7 @@ const config = require('../files/config/main.config')
 
 const reader = new nsq.Reader('instantloan__apply_loan', 'instantloan__apply',config.nsq)
 
-
 reader.connect()
-
-
 
 reader.on('message', msg => {
     //console.log('Received message [%s]: %s', msg.id, msg.body.toString())
