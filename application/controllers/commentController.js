7586fbc492a7b40870c1b684978b707cdf311d63
badajkawa <badajkawa@gmail.com>
2018-12-14 18:42:56 +0700

    log req

--- application/controllers/commentController.js
+++ application/controllers/commentController.js
@@ -3,11 +3,13 @@ var flow = require('flow-maintained');
 
 module.exports.fetch = function(req,res) {
 
+    console.log(req)
     var rreturn = {statusCode:200,message:'success',latency:0}
     res.json(rreturn).end()
 
 
 }
+
 module.exports.most_comment = function(req,res) {
 
     var rreturn = {statusCode:200,message:'success',latency:0};
