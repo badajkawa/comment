28300f5b9677205b7bccfd15ab122b16afc4fb97
badajkawa <45787924+badajkawa@users.noreply.github.com>
2018-12-14 14:32:01 +0700

    Init nsq (#14)
    * index nsq
    
    * git config email
    
    * nsq index Ok
    
    * tu
    
    * tu
    
    * nsq index Ok
    
    * tu

--- nsq/index.js
+++ nsq/index.js
@@ -1,12 +1,10 @@
 const nsq = require('nsqjs')
 const config = require('../files/config/main.config')
 
+
 const reader = new nsq.Reader('instantloan__apply_loan', 'instantloan__apply',config.nsq)
 
-
 reader.connect()
-
-
 
 reader.on('message', msg => {
     //console.log('Received message [%s]: %s', msg.id, msg.body.toString())
