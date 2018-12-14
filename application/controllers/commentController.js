7b89e3e83bfe04d3866ba066d6658a72f8377b68
badajkawa <badajkawa@gmail.com>
2018-12-14 18:41:12 +0700

    latency fetch

--- application/controllers/commentController.js
+++ application/controllers/commentController.js
@@ -3,7 +3,7 @@ var flow = require('flow-maintained');
 
 module.exports.fetch = function(req,res) {
 
-    var rreturn = {statusCode:200,message:'success'};
+    var rreturn = {statusCode:200,message:'success',latency:0}
     res.json(rreturn).end()
 
 
