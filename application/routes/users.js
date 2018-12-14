1937904502be40c9cb46a475ec70b39376e3a10e
badajkawa <badajkawa@gmail.com>
2018-12-14 19:04:06 +0700

    hello users

--- application/routes/users.js
+++ application/routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+
+router.get('/hello', function(req, res, next) {
+  res.send('hello');
+});
+
 module.exports = router;
