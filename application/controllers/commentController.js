6900c8282ab1c997f1f9b2898e82c59c63a35776
badajkawa <45787924+badajkawa@users.noreply.github.com>
2018-12-14 18:38:18 +0700

    Comment (#16)
    * init commmit
    
    * contrller init most
    
    * contrller init most

--- application/controllers/commentController.js
+++ application/controllers/commentController.js
@@ -8,3 +8,10 @@ module.exports.fetch = function(req,res) {
 
 
 }
+module.exports.most_comment = function(req,res) {
+
+    var rreturn = {statusCode:200,message:'success'};
+    res.json(rreturn).end()
+
+
+}
