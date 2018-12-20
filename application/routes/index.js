93b0c9c4cfd0ce7eeba8fbc6cf15b37922eec7a2
badajkawa <45787924+badajkawa@users.noreply.github.com>
2018-12-20 16:28:20 +0700

    Users up (#21)
    * hello users
    
    * hai
    
    * hai
    
    * -m

--- application/routes/index.js
+++ application/routes/index.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const nsq = require('nsqjs')
+const config = require('../../files/config/main.config')
+
+
+const reader = new nsq.Reader('instantloan__apply_loan', 'instantloan__apply',config.nsq)
+
+reader.connect()
+
+reader.on('message', msg => {
+  console.log('Received message [%s]: %s', msg.id, msg.body.toString())
+  //console.log('Received message')
+  msg.finish()
+})
+
+
+
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- application/routes/users.js
+++ application/routes/users.js
@@ -7,9 +7,13 @@ router.get('/', function(req, res, next) {
 });
 
 
-
 router.get('/hello', function(req, res, next) {
   res.send('hello');
+});
+
+
+router.get('/hai', function(req, res, next) {
+  res.send('hai');
 });
 
 module.exports = router;
